9ff6442a8a48994e200ca2957c6ca3cdce225c39
giostacchio <31801153+giostacchio@users.noreply.github.com>
2026-09-16 14:56:19 +0200

    Football Analyzer V16: refresh PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE = "football-analyzer-v15-20260911b";
+const CACHE = "football-analyzer-v16-20260916a";
 const CORE = [
-  "./", "./index.html", "./styles.css", "./app.js", "./v15-consensus.js", "./v15-ui-fix.js", "./manifest.webmanifest",
+  "./", "./index.html", "./styles.css", "./app.js", "./v16-personal-edge.js", "./v15-ui-fix.js", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-192.png",
   "./icon-maskable-512.png", "./apple-touch-icon.png", "./favicon.ico"
 ];
