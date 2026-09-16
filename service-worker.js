94a669a62449b66ae3eb62b3208a1d79d0d0f2c0
giostacchio <31801153+giostacchio@users.noreply.github.com>
2026-09-16 15:53:12 +0200

    Football V16.2: cache reliable UEFA loader

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE = "football-analyzer-v16-2-20260916";
+const CACHE = "football-analyzer-v16-2-20260916b";
 const CORE = [
-  "./", "./index.html", "./styles.css", "./app.js", "./v16-personal-edge.js", "./v15-ui-fix.js", "./v16-europe-all.js", "./manifest.webmanifest",
+  "./", "./index.html", "./styles.css", "./app.js", "./v16-personal-edge.js", "./v15-ui-fix.js", "./v162-uefa-reliable.js", "./v16-europe-all.js", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-192.png",
   "./icon-maskable-512.png", "./apple-touch-icon.png", "./favicon.ico"
 ];
